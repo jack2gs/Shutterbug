11c9f23863a2adf0c8e20cb0fb950051de6f8f14
jack2gs <sailor.g@163.com>
2016-07-26 18:50:45 +0800

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Gao Song on 7/26/16.
+//  Copyright © 2016 Gao Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Gao Song on 7/26/16.
+//  Copyright © 2016 Gao Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
